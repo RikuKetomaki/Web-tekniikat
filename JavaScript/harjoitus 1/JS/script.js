c5098976f01da2cacce6bf7e92e224769e429f88
RikuKetomaki <riku.ketomaki@gmail.com>
2022-11-04 15:03:28 +0200

    added collections 1-4

--- JavaScript/harjoitus 1/JS/script.js
+++ JavaScript/harjoitus 1/JS/script.js
@@ -1,3 +1,5 @@
+// Basics
+
 // 1
 console.log(document.getElementById("content").tagName);
 
@@ -14,3 +16,32 @@ console.log(third.textContent);
 // 4
 let fourth = document.querySelector("li:last-child");
 console.log(fourth.textContent);
+
+// Collections
+
+// 1
+let bodyDesc = document.querySelectorAll("body *");
+
+for( elem of bodyDesc){
+    console.log(elem.nodeName);
+}
+
+// 2
+let pElems = document.querySelectorAll("article p")
+for(elem of pElems){
+    console.log(elem.textContent)
+}
+
+// 3
+let liElems = document.querySelectorAll("li")
+for(elem of liElems){
+    elem.textContent = "John Doe"
+}
+
+// 4
+let collections4 = document.querySelectorAll("#content *");
+for(elem of collections4){
+    if(elem.textContent.length > 10){
+        console.log(elem.nodeName)
+    }
+}
