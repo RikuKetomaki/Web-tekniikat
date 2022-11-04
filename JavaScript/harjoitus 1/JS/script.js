cd869e4f76a35aa907f5593a7be67fe7775eeedf
RikuKetomaki <riku.ketomaki@gmail.com>
2022-11-04 13:55:43 +0200

    fixed //2 and added //4

--- JavaScript/harjoitus 1/JS/script.js
+++ JavaScript/harjoitus 1/JS/script.js
@@ -1,13 +1,16 @@
 // 1
-console.log(document.getElementById("content").tagName)
+console.log(document.getElementById("content").tagName);
 
 // 2
-let second = document.querySelector("#content")
-let second2 = document.querySelector(".basic")
 
-console.log(second.tagName)
-console.log(second2.tagName)
+let second2 = document.querySelector(".basic");
+
+console.log(second2.tagName);
 
 // 3
-let third = document.querySelector("ul .basic")
-console.log(third.textContent)
+let third = document.querySelector("ul .basic");
+console.log(third.textContent);
+
+// 4
+let fourth = document.querySelector("li:last-child");
+console.log(fourth.textContent);
